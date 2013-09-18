ab839ee3e073ddc7db6272ad38e192a3b8e0b552
Ernesto Cespedes <netosot@gmail.com>
2013-09-17 20:04:39 -0600

    nuevos cambios

--- Laboratorio1-3/pointer.cpp
+++ Laboratorio1-3/pointer.cpp
@@ -5,30 +5,34 @@ void square(int n){
   n=n*n;
   cout<<"Segun la funcion void square(int n) el valor de numero es: "<< n <<endl;
 }
-
 void square(int *ptr_n){
   *ptr_n=*ptr_n * *ptr_n;
   cout<<"Segun la funcion void square(int *ptr_n) el valor apuntado por ptr_n es: "<< *ptr_n <<endl;
 }
-
 	
 int main(void){
   int numero = 10, *ptr_num;
+  ptr_num=&numero ;//se asgna la direción de número al puntero ptr_n
+
+  //Funny printings
+  cout<<"el valor asignado al la variable nemero es: "<<numero<<endl;
+  cout<<"\nla dirección de memoria donde se almacena la varaible número: "<<&numero<<endl;
+  cout<<"\nla dirección a la que apunta el puntero ptr_num es: "<<ptr_num<<endl;
+
   //paso de parámetros por valor
   cout<<"El valor de la variable numero antes de la funcion void square (int) es:"<< numero <<endl;
   square(numero);
   cout<<"El valor de la varaible numero despues de la funcion void square (int) es:"<< numero <<endl;
 
   //Paso de parámetros por referencia
-  cout<< "\n El valor de la variable numero antes de la funcion void square(*int) es:"<< numero<<endl;
+  cout<< "\n El valor de la variable numero antes de la funcion void square(&int) es:"<< numero<<endl;
   square(&numero);
-  cout<< "El valor de la variable numero despues de la funcion void square (∗int) es : " <<numero<<endl;
+  cout<< "El valor de la variable numero despues de la funcion void square (&int) es : " <<numero<<endl;
 
   // Se puede manejar con punteros
-  ptr_num=&numero ;//se asgna la direción de número al puntero ptr_n
-  cout <<"\n El valor de la variable numero antes de la funcion void square (∗int) es : " <<*ptr_num <<endl;
+  cout <<"\n El valor de la variable numero antes de la funcion void square (ptr) es : " <<*ptr_num <<endl;
   square(ptr_num) ;
-  cout <<" El valor de la variable numero despues de la funcion void square (*int) es : " <<*ptr_num <<endl;;
+  cout <<" El valor de la variable numero despues de la funcion void square (ptr) es : " <<*ptr_num <<endl;;
   }
 
 
